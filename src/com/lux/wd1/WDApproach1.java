bd7c47b261c0c12b2ffc239eff20d70cf4db180e
OdysseyUA <odyssey.in.ua@gmail.com>
2015-01-09 13:17:29 +0200

    Demo class in work state.

--- src/com/lux/wd1/WDApproach1.java
+++ src/com/lux/wd1/WDApproach1.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.*;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
@@ -16,19 +18,29 @@ import org.openqa.selenium.firefox.internal.ProfilesIni;
 
 
 public class WDApproach1 {
-		
-	ProfilesIni profile = new ProfilesIni();	
-	WebDriver driver = new FirefoxDriver(profile.getProfile("hw"));
 	
-	@Before
-	public void setUp(){
+	private By byXpath = 
+			By.xpath(".//*[@id='mainMenuContainer']/ul//span[substring(., string-length() -6) = 'Search']");
+		
+	static ProfilesIni profile = new ProfilesIni();	
+	static WebDriver driver = new FirefoxDriver(profile.getProfile("hw"));
+	
+	@BeforeClass
+	public static void setUp(){
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.get("https://dev05.dev.hotwire.com:7002/ccc/loginPage");
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
+		driver.manage().window().setPosition(new Point(0,0));
+		driver.manage().window().setSize(new Dimension(960, 1035));
+		
+		
+		driver.findElement(By.xpath(".//*[@id='userName']")).sendKeys("csrcroz1");
+		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("Admin1234!");
+		driver.findElement(By.xpath(".//*[@value='Login']")).click();
 	}
 	
-	@After
-	public void tearDown(){
+	@AfterClass
+	public static void tearDown(){
 		driver.quit();
 	}
 	
@@ -36,18 +48,38 @@ public class WDApproach1 {
 	
 	
 	@Test
-	public void test1(){
-		driver.findElement(By.xpath(".//*[@id='userName']")).sendKeys("csrcroz1");
-		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("Admin1234!");
-		driver.findElement(By.xpath(".//*[@value='Login']")).click();
+	public void test1() {
+		
+		driver.switchTo().frame("c3Frame");
+
+		
+		if(driver.findElements(byXpath).size()!=0){
+			driver.findElement(byXpath).click();
+		}
+		else{			
+			System.out.println("No such element!");
+			return;
+		}
+			
 		
 		
-		driver.findElement(By.xpath(".//*[@id='mainMenuContainer']/ul/li[2]/a/span")).click();
-		System.out.println("test");
 		driver.findElement(By.xpath(".//*[@id='emailAddress']")).sendKeys("ansnitko@gmail.com");
 		driver.findElement(By.xpath(".//*[@name='Submit']")).click();
 		
 		assertEquals("Anastasiia A Snitko", driver.findElement(By.xpath(".//*[@id='listHolder']/ul/li[1]/span[2]")).getText());
+		
+		driver.switchTo().defaultContent();
+	}
+	
+	
+	@Test
+	public void test2() {
+						
+		driver.switchTo().frame("c3Frame");
+
+		assertTrue(driver.findElements(By.xpath(".//*[@id='tileName-A1']/table/tbody/tr/td/table[2]/tbody/tr/td[2]/div/div[3]")).size()!=0);			
+		
+		driver.switchTo().defaultContent();
 	}
 	
 	
